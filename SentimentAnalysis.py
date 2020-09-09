2f458e67782d8ef4029fcd5909507320addc6cd7
Neena Ugail <nhu21@bath.ac.uk>
2020-09-09 13:18:12 +0100

    added getSentiment() and peformed analysis
    added a function to get the sentiment of each tweet
    
    performed analysis to find out the percentage of positive, negative and neutral tweets

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,6 +1,7 @@
+import re
 import tweepy 
-from tweepy import OAuthHandler 
-from textblob import TextBlob 
+from tweepy.auth import OAuthHandler 
+from textblob import TextBlob
   
 class SentimentAnalysis(object): 
 
@@ -8,38 +9,105 @@ class SentimentAnalysis(object):
     def __init__(self): 
         
         #Keys and tokens from Twitter Developer portal
-        consumer_key = 'LkdUg86X0KIVLw9cmvq1CgdZy'
-        consumer_secret = '66jGHLbMmlPyNdOLC4utsXOw8D7F5Y4m4ge98QkSNAVLyqEMj7'
-        access_token = '456004005-4Cqe4mKXZSL1nFWl0e582lIjpK6j5iszH28AfS1c'
-        access_token_secret = 'iEMDvCyhsL0y5iZ2wgdJ1Y6Yj6StHDcN5pzzw8dYNm9Ze'
+        consumerKey = 'LkdUg86X0KIVLw9cmvq1CgdZy'
+        consumerSecret = '66jGHLbMmlPyNdOLC4utsXOw8D7F5Y4m4ge98QkSNAVLyqEMj7'
+        accessToken = '456004005-6IXIKd3E3ntjSjT57INTYqmb5hjThonirsxFBvmz'
+        accessTokenSecret = 'EclZSGVkxuw9NgKtFiJDu2Jo3zlWdWxVxklrn1h4Q4lgM'
   
         #Attempt authentication 
         try: 
-            self.authHandler = OAuthHandler(consumer_key, consumer_secret) 
-            self.authHandler.set_access_token(access_token, access_token_secret) 
+            self.authHandler = OAuthHandler(consumerKey, consumerSecret) 
+            self.authHandler.set_access_token(accessToken, accessTokenSecret) 
 
             #Create tweepy API object that will fetch tweets
-            self.api = tweepy.API(self.auth)
-
-            #Test authentication
-            api.verify_creditials()
+            self.api = tweepy.API(self.authHandler)
             print("Authentication worked")
 
         except: 
             print("Authentication Failed")
 
 
-    def getTweets():
-        #Number of tweets to be extracted
-        self.tweetNumber = 200
+    def getTweets(self, query, tweetNumber = 200):
 
-        tweets = api.user_timeline
+        #List to store the text of each tweet
+        content = []
+        
+        try:
+            #Fetch the tweets using the query given
+            fetchedTweets = self.api.search(q=query, count=tweetNumber)
 
-    def main():
-        api = SentimentAnalysis()
-        tweets = api.getTweets()
+            #Loop through tweets and store their text in the list
+            for t in fetchedTweets:
 
+                #Append tweet to list, ensuring it is only appended once
+                if t.retweet_count > 0:
+                    if t.text not in content:
+                        content.append(t.text)
+                else:
+                    content.append(t.text)
+                    
+            return content
+        
+        except tweepy.TweepError as e:
+            print("Error" + str(e))
 
+    #Function to remove links and special characters from tweets
+    #Taken from https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
+    def cleanTweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
+
+    def getSentiment(self, content):
+
+        #List to store the sentiment of each tweet
+        sentiments = []
+
+        for tweet in content:
+            blob = TextBlob(self.cleanTweet(tweet))
+
+            if blob.sentiment.polarity > 0:
+                sentiments.append('positive')
+            elif blob.sentiment.polarity == 0:
+                sentiments.append('neutral')
+            else:
+                sentiments.append('negative')
+            
+        return sentiments 
+
+def main():
+    api = SentimentAnalysis()
+
+    #Get tweets from twitter and find out their sentiment
+    tweetContent = api.getTweets(query = 'Donald Trump')
+    print(len(tweetContent), "tweets gathered")
+    tweetSentiment = api.getSentiment(tweetContent)
+
+    #List to store pairs of tweets and their sentiment
+    tweets = []
+
+    for i in range(len(tweetContent)-1):
+
+        #Dictionary to store the text and sentiment of each tweet
+        parameters = {}
+
+        parameters['text'] = tweetContent[i]
+        parameters['sentiment'] = tweetSentiment[i]
+
+        tweets.append(parameters)
+
+    #Select positive tweets and find percentage
+    posTweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    posPercentage = len(posTweets)/len(tweets) * 100
+    print("Positive tweets: ", round(posPercentage,2), "%")
+
+    #Select negative tweets and find percentage
+    negTweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    negPercentage = len(negTweets)/len(tweets) * 100
+    print("Negative tweets: ", round(negPercentage,2), "%")
+
+    #Find percentage of neutral tweets
+    neutralPercentage = (len(tweets) - (len(negTweets)+len(posTweets)))/len(tweets) * 100
+    print("Neutral tweets: ", round(neutralPercentage,2), "%")
+            
 #Call the main function
 if __name__ == "__main__": 
     main()
